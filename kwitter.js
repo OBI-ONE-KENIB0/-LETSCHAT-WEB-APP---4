73cea546b504dcc1bdc9e1c3dfcbd8d2b15db31a
OBI-ONE-KENIB0 <97593769+OBI-ONE-KENIB0@users.noreply.github.com>
2022-06-04 10:01:14 +1000

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,10 @@
+function addUser(){
+    user_name=document.getElementById("user_name").value;
+    localStorage.setItem("user_name",user_name);
+    window.location="kwitter.html";
+    }
+function logout(){
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+window.location="index.html";
+}
